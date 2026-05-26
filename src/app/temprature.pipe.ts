bc7b340a4ea2f244444666c2312998123852fe6a
Reda <mahmoudreda457@gmail.com>
2026-05-26 18:15:37 +0300

    accept params for custom pipe

--- src/app/temprature.pipe.ts
+++ src/app/temprature.pipe.ts
@@ -4,9 +4,37 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'temp',
   standalone: true,
 })
-
 export class TempraturePipe implements PipeTransform {
-  transform(value: any, ...args: any[]): any {
-    return value + ' - transformed'
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah',
+  ): any {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (9 / 5);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: ' C' | ' F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? ' C' : ' F';
+    } else {
+      symbol = outputType === 'cel' ? ' C' : ' F';
+    }
+
+    return `${outputTemp} ${symbol}`;
   }
 }
